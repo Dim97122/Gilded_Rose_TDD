2d4997fbea1e081109878846e9e736d7a7e1e102
Dim <dimitri.kiavue@gmail.com>
2020-05-20 17:09:34 +0200

    INITIAL COMMIT

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,2 @@
-import '@fortawesome/fontawesome-free/js/all.js';
-import "../sass/style.scss";
-
-import { routes } from "./routes";
-
-let pageArgument;
-
-const setRoute = () => {
-  let path = window.location.hash.substring(1).split("/");
-  pageArgument = path[1] || "";
-
-  var pageContent = document.getElementById("pageContent");
-  routes[path[0]](pageArgument);
-  return true;
-};
-
-window.addEventListener("hashchange", () => setRoute());
-window.addEventListener("DOMContentLoaded", () => setRoute());
-
-/////////////// MODULE DE RECHERCHE ///////////////////////////////
-const getGame = () => {
-  let gameName = document.getElementsByClassName('form-control')[0].value;
-  window.location.hash = `#gamelist/${gameName}`;
-}
-// const search_input = document.getElementsByClassName('form-control')[0]
-document.getElementsByClassName('form-control')[0].addEventListener("keydown", getGame)
-/////////////// MODULE DE RECHERCHE ///////////////////////////////
+import { Item } from './item';
+import { Shop } from './shop';
--- src/js/routes.js
+++
@@ -1,9 +0,0 @@
-import { Home } from "./Home";
-import { GameList } from "./GameList";
-import { GameDetails } from "./GameDetails";
-
-export const routes = {
-	"": Home,
-	gamelist: GameList,
-	gamedetails: GameDetails,
-};
